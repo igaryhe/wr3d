668471ee8ba8daf8b5d2b2e03d94dc812d701c2c
Dan <igaryhe@gmail.com>
2020-11-27 00:25:58 +0800

    implemented render pipeline

--- src/state.rs
+++ src/state.rs
@@ -12,6 +12,7 @@ pub struct State {
     sc_desc: wgpu::SwapChainDescriptor,
     swap_chain: wgpu::SwapChain,
     pub size: winit::dpi::PhysicalSize<u32>,
+    render_pipeline: wgpu::RenderPipeline,
 }
 
 impl State {
@@ -40,6 +41,58 @@ impl State {
             height: size.height,
             present_mode: wgpu::PresentMode::Fifo,
         };
+
+        let vs_module = device.create_shader_module(wgpu::include_spirv!("shader.vert.spv"));
+        let fs_module = device.create_shader_module(wgpu::include_spirv!("shader.frag.spv"));
+
+        let render_pipeline_layout = device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+            label: Some("Render Pipeline Layout"),
+            bind_group_layouts: &[],
+            push_constant_ranges: &[],
+        });
+
+        let render_pipeline = device.create_render_pipeline(
+            &wgpu::RenderPipelineDescriptor {
+                label: Some("Render Pipeline"),
+                layout: Some(&render_pipeline_layout),
+                vertex_stage: wgpu::ProgrammableStageDescriptor {
+                    module: &vs_module,
+                    entry_point: "main",
+                },
+                fragment_stage: Some(
+                    wgpu::ProgrammableStageDescriptor {
+                        module: &fs_module,
+                        entry_point: "main",
+                }),
+                rasterization_state: Some(
+                    wgpu::RasterizationStateDescriptor {
+                        front_face: wgpu::FrontFace::Ccw,
+                        cull_mode: wgpu::CullMode::Back,
+                        depth_bias: 0,
+                        depth_bias_slope_scale: 0.0,
+                        depth_bias_clamp: 0.0,
+                        clamp_depth: false,
+                    }
+                ),
+                color_states: &[
+                    wgpu::ColorStateDescriptor {
+                        format: sc_desc.format,
+                        color_blend: wgpu::BlendDescriptor::REPLACE,
+                        alpha_blend: wgpu::BlendDescriptor::REPLACE,
+                        write_mask: wgpu::ColorWrite::ALL,
+                    }
+                ],
+                primitive_topology: wgpu::PrimitiveTopology::TriangleList,
+                depth_stencil_state: None,
+                vertex_state: wgpu::VertexStateDescriptor {
+                    index_format: wgpu::IndexFormat::Uint16,
+                    vertex_buffers: &[],
+                },
+                sample_count: 1,
+                sample_mask: !0,
+                alpha_to_coverage_enabled: false,
+            });
+
         let swap_chain = device.create_swap_chain(&surface, &sc_desc);
         Ok(Self {
             surface,
@@ -48,6 +101,7 @@ impl State {
             sc_desc,
             swap_chain,
             size,
+            render_pipeline,
         })
     }
 
@@ -71,9 +125,8 @@ impl State {
         let mut encoder = self.device.create_command_encoder(
             &wgpu::CommandEncoderDescriptor { label: Some("Render Encoder") },
         );
-        {
-            let _render_pass = encoder
-                .begin_render_pass(&wgpu::RenderPassDescriptor {
+        let mut render_pass = encoder
+            .begin_render_pass(&wgpu::RenderPassDescriptor {
                 color_attachments: &[
                     wgpu::RenderPassColorAttachmentDescriptor {                        
                         attachment: &frame.view,
@@ -91,8 +144,9 @@ impl State {
                 ],
                 depth_stencil_attachment: None,
             });
-        }
-
+        render_pass.set_pipeline(&self.render_pipeline);
+        render_pass.draw(0..3, 0..1);
+        drop(render_pass);
         self.queue.submit(std::iter::once(encoder.finish()));
         Ok(())
     }
