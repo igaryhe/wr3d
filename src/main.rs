8cd9aa93863908dfd7fbd3fb1aa6fc933dc21ca4
Dan <igaryhe@gmail.com>
2020-11-26 20:35:13 +0800

    create window

---
+++ src/main.rs
@@ -0,0 +1,44 @@
+use winit::{
+    event::*,
+    event_loop::{ControlFlow, EventLoop},
+    window::WindowBuilder,
+};
+use anyhow::Result;
+fn main() -> Result<()> {
+    let event_loop = EventLoop::new();
+    let window = WindowBuilder::new().build(&event_loop)?;
+    event_loop.run(move |event, _, control_flow| {
+        *control_flow = ControlFlow::Poll;        
+        match event {
+            Event::WindowEvent {
+                ref event,
+                window_id,
+            } if window_id == window.id() => {
+                match event {
+                    WindowEvent::CloseRequested =>
+                        *control_flow = ControlFlow::Exit,
+                    WindowEvent::KeyboardInput {
+                        input,
+                        ..
+                    } => {
+                        match input {
+                            KeyboardInput {
+                                state: ElementState::Pressed,
+                                virtual_keycode: Some(VirtualKeyCode::Escape),
+                                ..
+                            } => *control_flow = ControlFlow::Exit,
+                            _ => {}
+                        }
+                    },
+                    _ => (),
+                }
+            },
+            Event::MainEventsCleared => {
+                window.request_redraw();
+            },
+            Event::RedrawRequested(_) => {
+            },
+            _ => (),
+        }
+    });
+}
